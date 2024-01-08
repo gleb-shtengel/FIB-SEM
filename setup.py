17b965bdbc38f0b3e29051a2084bf8715706c5c7
Gleb Shtengel <gleb.shtengel@gmail.com>
2024-01-08 17:05:04 -0500

    update setup.py
    get version number from __init__.py

--- setup.py
+++ setup.py
@@ -9,6 +9,21 @@ Copyright (c) 2024, gleb.shtengel@gmail.com
 
 import setuptools
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+version=get_version("SIFT_gs/__init__.py")
+
 setuptools.setup(
     name="FIBSEM_gs",
     version='4.0.1',
