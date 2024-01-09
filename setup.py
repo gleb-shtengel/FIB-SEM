33aaabfe661c48150f48b193deb461ae2bde98e5
Gleb Shtengel <gleb.shtengel@gmail.com>
2024-01-09 10:07:59 -0500

    Update setup.py
    Read version from __init__.py

--- setup.py
+++ setup.py
@@ -8,25 +8,23 @@ Copyright (c) 2024, gleb.shtengel@gmail.com
 """
 
 import setuptools
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
+import codecs
+import os.path
 
 def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
+     with codecs.open(rel_path, 'r') as fp:
+        for line in fp.read().splitlines():
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
 
-#version=get_version("SIFT_gs/__init__.py")
+version=get_version("SIFT_gs/__init__.py")
 
 setuptools.setup(
     name="FIBSEM_gs",
-    version='4.0.1',
+    version=version,
     author="Gleb Shtengel",
     author_email="gleb.shtengel@gmail.com",
     url="https://github.com/gleb-shtengel/FIB-SEM",
