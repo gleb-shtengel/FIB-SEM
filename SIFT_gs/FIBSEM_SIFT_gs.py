735746df83d55637f5cbb53db3520d3345a31ab3
Gleb Shtengel <gleb.shtengel@gmail.com>
2025-01-16 13:51:45 -0500

    bug fix

--- SIFT_gs/FIBSEM_SIFT_gs.py
+++ SIFT_gs/FIBSEM_SIFT_gs.py
@@ -555,11 +555,11 @@ def find_autocorrelation_peak(ind_acr, mag_acr, **kwargs):
         di = aperture//2
         ACR_nozero = np.concatenate((mag_acr[sz//2-di : sz//2], mag_acr[sz//2 : sz//2+di+1]))
         lags_nozero = np.concatenate((ind_acr[sz//2-di : sz//2], mag_acr[sz//2 : sz//2+di+1]))
-        amp = np.max(ACR_nozero)-np.min(ACR_nozero)
-        mean = 0
-        sigma = 10.0
-        offset = 2.0 * np.min(ACR_nozero) - np.max(ACR_nozero)
-        popt, pcov = curve_fit(gauss_with_offset, lags_nozero, ACR_nozero, p0=[amp, mean, offset, sigma])
+        amp_guess = np.max(ACR_nozero)-np.min(ACR_nozero)
+        center_guess = 0
+        sigma_guess = 10.0
+        offset_guess = 2.0 * np.min(ACR_nozero) - np.max(ACR_nozero)
+        popt, pcov = curve_fit(gauss_with_offset, lags_nozero, ACR_nozero, p0=[amp_guess, center_guess, offset_guess, sigma_guess])
         mag_NFacr = popt[0]+popt[2]
     else:
         if extrapolate_signal == 'parabolic':
